7896f63ec955ce6b73a76cf58c18586aacea58bb
Yaser <kalkanyaser@gmail.com>
2024-06-08 15:59:19 +0300

    add agent.py
    Add function to perform search queries using Google Serper API

---
+++ mysite/socialapp/agent.py
@@ -0,0 +1,16 @@
+import http.client
+import json
+
+def search_query(query):
+    conn = http.client.HTTPSConnection("google.serper.dev")
+    payload = json.dumps({
+        "q": query
+    })
+    headers = {
+        'X-API-KEY': '8d463e31655d5eb6fe981010941110a876e845c3',
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/search", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))
